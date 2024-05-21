42f6558488c6732ae6259e289163026784435cf2
Pavel Asafov <pavelaof@gmail.com>
2024-05-21 13:43:40 +0300

    create a simple voiceover loop

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
-import { detectSounds } from './constants.js'
+import { detectSounds, points } from './constants.js'
 
 let playBtn = document.getElementById('playBtn')
 let curPointCont = document.getElementById('currentPoint')
+
+let isPlaying = false
+let currentIndex = 0
+let currentPoint, knittingId, letter, number
+let pointDelay = 3000
+let letAndNumDelay = 400
 
 let sound = new Howl({
   src: ['./audio/wallstingpoints.mp3'],
   sprite: detectSounds,
 })
 
+function playSound() {
+  sound.play(letter)
+  setTimeout(() => sound.play(number), letAndNumDelay)
+}
+
+function startKnitting() {
+  currentPoint = points[currentIndex]
+  if (!currentPoint) return
+  letter = currentPoint.charAt(0)
+  number = currentPoint.slice(1)
+
+  curPointCont.textContent = currentPoint
+
+  playSound()
+
+  currentIndex++
+
+  if (currentIndex == points.length) {
+    stopKnitting()
+  }
+}
+
+function stopKnitting() {
+  playBtn.textContent = 'Play'
+
+  sound.stop(letter)
+  sound.stop(number)
+
+  clearTimeout(knittingId)
+
+  isPlaying = false
+}
+
 function togglePlay() {
-  sound.play('10')
+  if (!isPlaying) {
+    curPointCont.textContent =
+    playBtn.textContent = 'Pause'
+    isPlaying = true
+
+    startKnitting()
+
+    knittingId = setTimeout(function interval() {
+      startKnitting()
+
+      knittingId = setTimeout(interval, pointDelay)
+    }, pointDelay)
+  } else {
+    stopKnitting()
+  }
 }
 
 playBtn.addEventListener('click', togglePlay)
-
-alert(Howler.usingWebAudio);
